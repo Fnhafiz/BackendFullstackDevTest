de71e305453d8a1fe60c319799798a7a06f44722
FnHafiz <farhanhafiz55@gmail.com>
2023-08-23 23:08:14 +0700

    feat: add loginregister route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
+import LoginRegisterRoutes from "./routes/LoginRegisterRoutes.js";
 
 const app = express();
 const port = 3001;
@@ -8,6 +9,8 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", LoginRegisterRoutes);
 
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
